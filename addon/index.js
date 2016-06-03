8aa1fa35547fe65bf197ea2adda9d46be78a234f
Nick Iaconis <niaconis@linkedin.com>
2016-06-03 14:17:28 -0700

    Create regex only once

--- addon/index.js
+++ addon/index.js
@@ -1,8 +1,10 @@
 import find from './find';
 
+const CONFIG_REGEX = /^[^/]+\/config\/environment$/;
+
 export function findConfigName(entries) {
   return find.call(Object.keys(entries), (entry) => {
-    return entry.match(/^[^/]+\/config\/environment$/);
+    return CONFIG_REGEX.test(entry);
   });
 }
 
