3ee4aa3649c055410484afcaa279d56d2127fc44
null null null <null@revolution.cheap>
2016-07-29 15:51:46 -0700

    stop using require, start using node

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* jshint node: true */
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-  name: 'ember-get-config'
+  name: 'ember-get-config',
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    const appConfig = app.project.config(process.env.EMBER_ENV) || {};
+
+    fs.writeFileSync(
+      path.join(__dirname, 'addon', 'index.js'),
+      `export default ${JSON.stringify(appConfig, null, 2)};`,
+      'utf-8'
+    );
+  }
 };
