14521209240faad8d7eb235f167a4ec0235ac4bd
null null null <null@revolution.cheap>
2016-08-01 10:30:15 -0700

    further es6 removal

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var path = require('path');
 module.exports = {
   name: 'ember-get-config',
 
-  included(app) {
+  included: function(app) {
     this._super.included.apply(this, arguments);
 
     var appConfig = app.project.config(process.env.EMBER_ENV) || {};
 
     fs.writeFileSync(
       path.join(__dirname, 'addon', 'index.js'),
-      `export default ${JSON.stringify(appConfig, null, 2)};`,
+      'export default ' + JSON.stringify(appConfig, null, 2) + ';',
       'utf-8'
     );
   }
