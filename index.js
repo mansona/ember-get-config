01e4322a60507a804a8b66f9b32f763a46a874e9
null null null <null@revolution.cheap>
2016-08-11 14:22:35 -0700

    stop copying wholee config

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ module.exports = {
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    var appConfig = app.project.config(process.env.EMBER_ENV) || {};
+    while (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    var modulePrefix = app.project.config(process.env.EMBER_ENV)['modulePrefix'];
 
     fs.writeFileSync(
       path.join(__dirname, 'addon', 'index.js'),
-      'export default ' + JSON.stringify(appConfig, null, 2) + ';',
+      'export { default } from \'' + modulePrefix + '/config/environment\';',
       'utf-8'
     );
   }
