65981b61ab57675e25a64f56e1d1afc08135b7b2
null null null <null@affinitygamescollective.org>
2016-08-14 15:32:35 -0700

    ensure addon dir

--- index.js
+++ index.js
@@ -15,9 +15,16 @@ module.exports = {
     }
 
     var modulePrefix = app.project.config(process.env.EMBER_ENV)['modulePrefix'];
+    var addonDir = path.join(__dirname, 'addon');
+
+    try {
+      fs.statSync(addonDir);
+    } catch(e) {
+      fs.mkdirSync(addonDir);
+    }
 
     fs.writeFileSync(
-      path.join(__dirname, 'addon', 'index.js'),
+      path.join(addonDir, 'index.js'),
       'export { default } from \'' + modulePrefix + '/config/environment\';',
       'utf-8'
     );
