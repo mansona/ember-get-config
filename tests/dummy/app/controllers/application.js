ca0fe421d7959e2aa7e7ccd6c6131b9f92481006
David Mazza <davidedwardmazza@gmail.com>
2019-12-16 22:09:39 -0800

    revert migration to native class syntax to support Ember 3.4

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import config from 'ember-get-config';
 
-export default class ApplicationController extends Controller {
-  foo = config.foo
-}
+export default Controller.extend({
+  foo: config.foo
+});
