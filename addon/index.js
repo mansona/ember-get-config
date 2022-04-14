3d888a01b42d648051f7b1388b6032e6ffc0053d
Josemar Luedke <josemarluedke@gmail.com>
2022-04-13 17:49:49 -0700

    Use ternary instead of if statement in module export

--- addon/index.js
+++ addon/index.js
@@ -1,11 +1,5 @@
 import { isTesting, macroCondition, getOwnConfig } from '@embroider/macros';
 
-let config;
-
-if (macroCondition(isTesting())) {
-  config = getOwnConfig().testConfig;
-} else {
-  config = getOwnConfig().config;
-}
-
-export default config;
+export default macroCondition(isTesting())
+  ? getOwnConfig().testConfig
+  : getOwnConfig().config;
