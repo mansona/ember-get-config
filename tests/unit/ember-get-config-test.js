3c69fe32bf5db9548e9437cbb7471ead110a4648
Bert De Block <bert.deblock@bagaar.be>
2022-04-22 21:01:41 +0200

    Add failing tests under Embroider

--- tests/unit/ember-get-config-test.js
+++ tests/unit/ember-get-config-test.js
@@ -1,3 +1,4 @@
+import configApp from 'dummy/config/environment';
 import config from 'ember-get-config';
 import { module, test } from 'qunit';
 
@@ -5,4 +6,8 @@ module('ember-get-config');
 
 test('it exports the app config file', function (assert) {
   assert.equal(config.environment, 'test');
+});
+
+test("it exports a reference to the app's config", function (assert) {
+  assert.equal(config, configApp);
 });
